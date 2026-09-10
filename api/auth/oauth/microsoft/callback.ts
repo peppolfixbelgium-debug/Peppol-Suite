1d8aae71f444df8a0bea64258bc9d18eee8fb3c1
peppolfixbelgium-debug <peppolfixbelgium@gmail.com>
2026-09-10 16:38:35 +0200

    Route Microsoft OAuth callback through hardened auth handler

--- api/auth/oauth/microsoft/callback.ts
+++ api/auth/oauth/microsoft/callback.ts
@@ -1,2 +1,1 @@
-import { callbackOAuth } from "../../../_lib/oauth";
-export default async function handler(request:Request){try{return await callbackOAuth(request,"microsoft");}catch{return new Response(null,{status:302,headers:{location:"/login?error=oauth"}});}}
+export { default } from "../../[...path]";
