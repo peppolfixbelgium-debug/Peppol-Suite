c64bd5e5205dbe4596e56083b3b9dc1035299ce3
peppolfixbelgium-debug <peppolfixbelgium@gmail.com>
2026-09-10 16:38:15 +0200

    Route OAuth start through hardened auth handler

--- api/auth/oauth/google/start.ts
+++ api/auth/oauth/google/start.ts
@@ -1,2 +1,1 @@
-import { startOAuth } from "../../../_lib/oauth";
-export default async function handler(){try{return await startOAuth("google");}catch{return new Response(null,{status:302,headers:{location:"/login?error=oauth"}});}}
+export { default } from "../../[...path]";
